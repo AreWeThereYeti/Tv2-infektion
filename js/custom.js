7896f0d3b7a0085a92506a03e6287c52ea4f1b41
Andreas Sprotte-Hansen <andreassprotte-hansen@andreassprottelocal.local>
2013-02-22 16:13:36 +0100

    dynamically creating map layer

--- js/custom.js
+++ js/custom.js
@@ -3,25 +3,97 @@ console.log("document not ready");
 
 $(document).ready(function() {
 	console.log("document ready");
+	console.log('creating visualization');
+	
+	layer_url='http://tv2.cartodb.com/api/v1/viz/3839/viz.json';
+	
+  // window.map=cartodb.createVis('map', 'http://tv2.cartodb.com/api/v1/viz/3839/viz.json', {
+  //     shareable: true,
+  //     title: true,
+  //     description: true,
+  //     search: false,
+  //     tiles_loader: true,
+  //     center_lat: 56,
+  //     center_lon: 10,
+  //     zoom: 7
+  // }).done(function(vis, layers) {
+  //   // layer 0 is the base layer, layer 1 is cartodb layer
+  //   layers[1].on('featureOver', function(e, pos, latlng, data) {
+  //     cartodb.log.log(e, pos, latlng, data);
+  //   });
+  //   // you can get the native map to work with it
+  //   // depending if you use google maps or leaflet
+  //   window.map = vis.getNativeMap();
+  //   // map.setZoom(3)
+  //   // map.setCenter(new google.maps.Latlng(...))
+  // }).error(function(err) {
+  //   console.log(err);
+  // });
+
+	var map = new L.Map('map', { 
+	    // shareable: true,
+	    // title: true,
+	    // description: true,
+	    // search: false,
+	    // tiles_loader: true,
+	    zoom: 7,
+	    center: [56,10]
+	  });
+	L.tileLayer('https://dnv9my2eseobd.cloudfront.net/v3/cartodb.map-4xtxp73f/{z}/{x}/{y}.png', {
+	  attribution: 'Mapbox <a href="http://mapbox.com/about/maps" target="_blank">Terms & Feedback</a>'
+	}).addTo(map);
 
 	$("#graphcontainer").click(function() {
-		alert("Handler for .click() called.");
 		
-		cartodb.createLayer(map, {
-		    type: 'cartodb',
-		    options: {
-		        table: 'infektionskort',
-		        user_name: 'TV2',
-		        query: 'select * from infektionskort where cartodb_id > 10000'
-		    }
-			}).on('done',function(layer) {
-			    map.addLayer(layer);
-			}).on('error',function(err){
-				console.log('error');
-				console.log(err);
-		});
+		cartodb.createLayer(map,{
+				type: 'cartodb',
+				options: {
+	        table: 'infektionskort',
+	        user_name: 'TV2',
+	        query: 'select * from infektionskort where cartodb_id > 90',
+					interactivity: "cartodb_id,the_geom,magnitude"
+				}
+	    })
+			.on('done',function(layer) {
+				console.log('done and adding layer');
+				console.log(layer);
+				layer.setInteraction(true);
+				layer.on('featureOver', function(e, latlng, pos, data){
+					console.log('featureOver ran');
+				})
+				.on('error', function(err) {
+	        console.log('error: ' + err);
+	      });
+				console.log(layer);
+			  map.addLayer(layer);
+			})
+			.on('error', function(err) {
+	    	alert("some error occurred: " + err);
+			})
+
 	});
 });
+
+
+
+// $("#graphcontainer").click(function() {
+// 	console.log('creating layer');
+// 	cartodb.createLayer(window.map, {
+// 	    type: 'cartodb',
+// 	    options: {
+// 	        table: 'infektionskort',
+// 	        user_name: 'TV2',
+// 	        query: 'select * from infektionskort where cartodb_id > 90'
+// 	    }
+// 		}).on('done',function(layer) {
+// 			console.log('done and adding layer');
+// 			console.log(layer);
+// 		  map.addLayer(layer);
+// 		}).on('error',function(err){
+// 			console.log('error');
+// 			console.log(err);
+// 	});
+// });
 
 
 /*
